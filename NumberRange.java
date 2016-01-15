16777c37d84908d2d99fd7887613b8555eacd931
Josh Humpherys <joshua.humpherys@gmail.com>
2016-01-14 22:53:08 -0500

    Parse lines of file to obtain ranges

--- NumberRange.java
+++ NumberRange.java
@@ -21,13 +21,51 @@ public class NumberRange {
             br = new BufferedReader(new FileReader(args[0]));
             String line;
             while((line = br.readLine()) != null) {
-                System.out.println(line);
+                list.add(getRange(line));
             }
         }
         catch(IOException e) {
             System.err.println("Error: Unable to find or read file.");
             System.err.println("Please make sure the file exists and can be read.");
             return;
+        }
+    }
+
+    public void add(Range range) {
+         System.out.println(range);
+    }
+
+    private static Range getRange(final String input) {
+        if(input.indexOf("//") == 0) return null;
+        String line = input.trim();
+        if(line.length() == 0) return null;
+        try {
+            int index = line.indexOf(" ");
+            if(index == -1) return new Range(Long.parseLong(line));
+            long start = Long.parseLong(line.substring(0, index));
+            long end = Long.parseLong(line.substring(index + 1).trim());
+            return new Range(start, end);
+        }
+        catch(NumberFormatException e) {
+            System.err.println("Error: Unable to parse line of file.");
+            System.err.println("Contents of line: " + input);
+            return null;
+        }
+    }
+
+    private static class Range {
+        protected long start, end;
+        Range(long start, long end) {
+            this.start = start;
+            this.end = end;
+        }
+        Range(long startAndEnd) {
+            start = end = startAndEnd;
+        }
+
+        @Override
+        public String toString() {
+            return start == end ? String.valueOf(start) : start + "-" + end;
         }
     }
 }
