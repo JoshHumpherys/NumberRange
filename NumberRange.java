2b73a9f22db3c416e90f1027de5a2c3662e91963
Josh Humpherys <joshua.humpherys@gmail.com>
2016-01-14 22:03:44 -0500

    Update description

--- NumberRange.java
+++ NumberRange.java
@@ -1,4 +1,6 @@
-// Josh Humpherys
+// NumberRange.java
+// Developed by Josh Humpherys as Assignment0 for COS230
+// Reads a list of number ranges and prints the combined ranges
 
 public class NumberRange {
     public static void main(String[] args) {
